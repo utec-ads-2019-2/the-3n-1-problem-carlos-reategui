27dc965e8bf6c562850bc491442397805894f2b0
carlos-reategui <carlos.reategui@utec.edu.pe>
2019-08-24 12:50:47 -0500

    accepted

--- main.cpp
+++ main.cpp
@@ -1,63 +1,21 @@
 #include <iostream>
-#include <stdio.h>
-using namespace std;
 
-unsigned int cycleLength(unsigned int n) {
-    unsigned int count = 0;
-    do {
-        count++;
-        if (n == 1)
-            break;
-
-        if (n % 2 == 0)
-            n /= 2;
-        else
-            n = n * 3 + 1;
-    } while (n >= 1);
-    return count;
-}
-
-void cyclePrint(unsigned int n) {
-    do {
-        if (n == 1) {
-            cout << 1;
-            break;
-        } else
-            cout << n << " ";
-
-        if (n % 2 == 0)
-            n /= 2;
-        else
-            n = n * 3 + 1;
-    } while (n >= 1);
-}
-
-unsigned int maxCycleLength(unsigned int n1, unsigned int n2) {
-    unsigned int maxLength = 0;
-    for (unsigned int i = n1; i <= n2; i++) {
-        unsigned int temp = cycleLength(i);
-        if(temp > maxLength)
-            maxLength = temp;
-    }
-
-    return maxLength;
-}
-
-using namespace std;
-int main() {
-
-    int inputList[4][2];
-    for(auto & i : inputList) {
-        int n1, n2;
-        do {
-            scanf("%d %d", &n1, &n2);
-        } while(n1 == 0 & n1 >= 10000 & n2 == 0 & n2 >= 10000);
-        i[0] = n1;
-        i[1] = n2;
-    }
-
-    for(auto & i : inputList) {
-        cout << i[0] << " " << i[1] << " " << maxCycleLength(i[0], i[1]) << endl;
+int main(){
+    int i, j;
+    while((scanf("%u %u", &i, &j)) == 2){
+        unsigned int maxCycleLength = 0, i2, j2;
+        i2 = i<=j ? i:j;
+        j2 = i<=j ? j:i;
+        for(int n = i2; n <= j2; n++){
+            int copyOfn = n, cycleLength = 1;
+            while(copyOfn != 1){
+                if(copyOfn % 2) copyOfn = 3 * copyOfn + 1;
+                else copyOfn /= 2;
+                cycleLength++;
+            }
+            if(maxCycleLength < cycleLength) maxCycleLength = cycleLength;
+        }
+        printf("%u %u %u\n", i, j, maxCycleLength);
     }
     return 0;
 }
